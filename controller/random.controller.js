48606a85f1761d0b9a0563271e2879284509c0cb
Hakan Dursun <68669489+hakandrsn@users.noreply.github.com>
2024-03-01 13:12:56 +0300

    random method empty lsit fix

--- controller/random.controller.js
+++ controller/random.controller.js
@@ -1,17 +1,18 @@
 const createHttpError = require("http-errors");
-const Activity = require("../model/activity.model");
-const Challenge = require("../model/challenge.model");
-const Food = require("../model/food.model");
-const Game = require("../model/game.model");
-const Hobby = require("../model/hobby.model");
-const Travel = require("../model/travel.model");
 const { getRandomModel } = require("../utils/methods");
-
+const { isEmpty } = require("lodash");
 
 const randomActivity = async (req, res, next) => {
   const body = req.body;
   try {
-    const response = await getRandomModel(body);
+    const randomGetterMethod = async () => {
+      let randomAct = await getRandomModel(body);
+      if (randomAct.length < 1) {
+        return randomGetterMethod();
+      }
+      return randomAct;
+    };
+    let response =await randomGetterMethod();
     if (!response) {
       return createHttpError(404, "random_can't_find");
     }
